a2c618d0db76610e8c5bb6a48e7c710d1ca1fa79
agusputra <agus.putra99@gmail.com>
2018-12-15 22:15:42 +0700

    make it neater

--- number-1.js
+++ number-1.js
@@ -1,12 +1,13 @@
-// Given an array [1,4,5,7,12, 19, 45, 101], write a function that returns array that satisfies this condition: x*2 - (5 - x) == even number
+
+// Given an array [1, 4, 5, 7, 12, 19, 45, 101], write a function that returns array that satisfies this condition: x*2 - (5 - x) == even number
 
 function f(arr) {
-  var result = []
+  const result = []
   for (var i in arr) {
-    var x = arr[i]
-    var y = x*2-(5-x)
-    if (y%2 === 0) 
+    const x = arr[i]
+    const y = x * 2 - (5 - x)
+    if (y % 2 === 0)
       result.push(x)
   }
-  return result;
+  return result
 }
